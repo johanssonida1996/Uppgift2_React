0eb20623f93ad2280dcda49c73d89c5c91bd457c
johanssonida1996@gmail.com <johanssonida1996@gmail.com>
2021-01-19 14:40:21 +0100

    final commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from '../assets/images/logo.png'
+
+const Navbar = () => {
+    return (       
+         
+    <nav className="navbar navbar-light bg-light">
+        <div className="container">
+            <a className="navbar-brand d-flex align-items-center" href="/">
+            <img src={logo} alt="" width="30" height="24" className="d-inline-block align-top" /> 
+            <span className="ms-2">To-Do list</span>
+            </a>
+         </div>
+    </nav>
+      
+    )
+}
+
+export default Navbar
